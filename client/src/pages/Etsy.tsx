37282320c16a283134cce76dfe9ccffcb5b0e25e
avnik <avnikolaou@hotmail.com>
2023-03-28 10:56:14 +0300

    bug: connect to etsy button in heroku

--- client/src/pages/Etsy.tsx
+++ client/src/pages/Etsy.tsx
@@ -94,19 +94,6 @@ const Etsy = () => {
         Login to Etsy
       </a>
 
-      <a
-        href={`https://www.etsy.com/oauth/connect?
-          response_type=code
-          &redirect_uri=${process.env.ETSY_AUTH_CALLBACK}
-          &scope=transactions_r%20transactions_w%20profile_r
-          &client_id=${process.env.ETSY_API_KEY}&state=${credentials.state}
-          &code_challenge=${credentials.codeChallenge}
-          &code_challenge_method=S256`}
-        className="bg-black hover:bg-gray-700 text-white font-bold py-2 px-4 rounded"
-      >
-        Login to Etsy
-      </a>
-
       <button
         className="bg-black hover:bg-gray-700 text-white font-bold py-2 px-4 rounded"
         onClick={handleRefreshButtonClick}
